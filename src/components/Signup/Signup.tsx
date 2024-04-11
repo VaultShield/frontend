92b86814570da5d04d4f6bd546f4f6d4d9fe3cfd
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-04-11 18:20:38 -0500

    feat: validations in signup

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -3,6 +3,11 @@ import bcrypt from 'bcryptjs';
 import { card, insideCard, btnDefault } from 'styles/tailwind.classes';
 import InputBase from 'components/InputBase';
 import { UserContext } from 'contexts/userContext';
+import {
+  validateEmail,
+  validatePassword,
+  validateForm
+} from 'utils/validations';
 
 const Signup = () => {
   //context
@@ -10,20 +15,32 @@ const Signup = () => {
   //user variables
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  //validation error;
+  const [errors, setErrors] = useState({});
 
   const handleRegister: MouseEventHandler<HTMLButtonElement> = async (
     event
   ) => {
     try {
       event.preventDefault();
-      const passwordHash = bcrypt.hashSync(password, 10);
-      const newUser = {
-        email,
-        password: passwordHash
-      };
-      await addUser(newUser);
+
+      const errorsForm = validateForm([
+        { name: 'email', value: email, required: true },
+        { name: 'password', value: password, required: true, minLength: 8 }
+      ]);
+
+      setErrors((errors) => errorsForm);
+
+      if (Object.keys(errorsForm).length === 0) {
+        const passwordHash = bcrypt.hashSync(password, 10);
+        const newUser = {
+          email,
+          password: passwordHash
+        };
+        await addUser(newUser);
+      }
     } catch (e) {
-      //erro handler
+      setErrors('An error occurred');
     }
   };
 
@@ -36,7 +53,6 @@ const Signup = () => {
           </h2>
           <p className="dark:text-gray-100"> one account for everything!</p>
         </div>
-
         <InputBase
           label="Email"
           type="email"
@@ -44,13 +60,14 @@ const Signup = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        {errors.email && <p className="text-red-500">{errors.email}</p>}{' '}
         <InputBase
           label="Password"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-
+        {errors.password && <p className="text-red-500">{errors.password}</p>}{' '}
         <button className={btnDefault} onClick={handleRegister}>
           Create Account
         </button>
