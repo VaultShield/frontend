d23ac7838c70d598ac6dfd2cc12afef08b2e4fba
Diego <josediegoferia@gmail.com>
2024-04-13 10:20:52 -0300

    feat: component menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,11 @@
-import { Anchor } from 'components/Anchor';
+import { Menu } from 'components/Menu';
 import { GithubIcon } from 'components/svg/GithubIcon';
 export function Home() {
   return (
     <div className="text-white border-b-2 border-violet-800 w-full h-full">
       <div className="">
         <header className="bg-indigo-950 h-14 flex justify-between p-4">
-          <div className="w-[172px] flex">VaultShield</div>
-          <ul>
-            <li>
-              <a className="hover:underline text-cinder-100" href="/">
-                Home
-              </a>
-            </li>
-          </ul>
-          <ul className="flex gap-4 w-[172px]">
-            <li>
-              <Anchor direction="/login" name="Login" />
-            </li>
-
-            <li>
-              <Anchor direction="/signup" name="Signup" />
-            </li>
-          </ul>
+          <Menu />
         </header>
         <main className="h-[600px] flex justify-center items-center">
           <section className="flex flex-col gap-4">
