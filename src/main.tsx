eb6d6db069a4ccb32dbeb3aacb10607da4c253b8
Maje <mjnct@hotmail.com>
2024-05-03 18:07:45 +0200

    feature: removed unused userContext files and reference

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { UserContextProvider } from 'contexts/userContext';
 import { ThemeContextProvider } from 'contexts/themeContext';
-import { NotificationContextProvider } from 'context/notificationContext';
-
 import App from './App';
 import './index.css';
 import { NotificationContextProvider } from 'contexts/notificationContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <ThemeContextProvider>
-        <NotificationContextProvider>
-          <Router>
-            <App />
-          </Router>
-        </NotificationContextProvider>
-      </ThemeContextProvider>
-    </UserContextProvider>
+    <ThemeContextProvider>
+      <NotificationContextProvider>
+        <Router>
+          <App />
+        </Router>
+      </NotificationContextProvider>
+    </ThemeContextProvider>
   </React.StrictMode>
 );
