b7b666669077226298b89672b39fa033a5e8abdc
Maje <mjnct@hotmail.com>
2024-05-03 17:59:18 +0200

    feature: created Login hook and save and recover user in sesion storage

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,88 +1,14 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import LoginRegister from 'services/LoginRegister';
-import { toast } from 'sonner';
-import { useUserStore } from 'store/userStore';
 import { btnDefault } from 'styles/tailwind.classes';
-import { LoginRequest, User } from 'types/apiTypes';
-import { validateForm } from 'utils/validations';
 import InputBase from '../InputBase';
-
-interface ErrorsForm {
-  username?: string;
-  password?: string;
-  error?: string;
-}
+import { useLogin } from 'hooks/useLogin';
 
 interface LoginProps {
   handleLogin: () => void;
 }
+
 export function Login({ handleLogin }: LoginProps) {
-  const setUser = useUserStore((state) => state.setUser);
-  const setTokenStore = useUserStore((state) => state.setToken);
-  const setIsLogged = useUserStore((state) => state.setIsLogged);
-  const setRefreshToken = useUserStore((state) => state.setRefreshToken);
-
-  const navigate = useNavigate();
-  const [infoUser, setInfoUser] = useState({
-    username: '',
-    password: ''
-  });
-  const [errors, setErrors] = useState<ErrorsForm>({});
-
-  const sendData = async (infoUser: LoginRequest) => {
-    try {
-      const errorsForm: ErrorsForm = validateForm([
-        { name: 'userName', value: infoUser.username, required: true },
-        {
-          name: 'password',
-          value: infoUser.password,
-          required: true,
-          minLength: 8
-        }
-      ]);
-
-      setErrors(errorsForm);
-      if (!errorsForm.username && !errorsForm.password) {
-        await loginUser(infoUser);
-        navigate('/');
-      }
-    } catch (err) {
-      if (err instanceof Error) setErrors({ error: err.message });
-    }
-  };
-
-  const handleSignupClick = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-    handleLogin();
-  };
-
-  const loginUser = async (credentials: LoginRequest): Promise<void> => {
-    const response = await LoginRegister.login(credentials);
-    const { token, user, refreshToken } = response;
-    if (token) {
-      localStorage.setItem('token', JSON.stringify(token)); // Store the user token in local storage
-      saveGlobalStateUser(token, user, refreshToken);
-      toast.success('Login successful', {
-        duration: 2000
-      });
-    } else {
-      throw new Error('Incorrect username or password');
-    }
-  };
-
-  const saveGlobalStateUser = (
-    token: string,
-    user: User,
-    refreshToken: string
-  ) => {
-    setTokenStore(token);
-    setUser(user);
-    setRefreshToken(refreshToken);
-    setIsLogged(true);
-  };
+  const { infoUser, errors, sendData, handleSignupClick, setInfoUser } =
+    useLogin({ handleLogin });
 
   return (
     <div className="flex flex-col justify-center items-center p-0 m-0 ">
---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,118 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { validateForm } from 'utils/validations';
+import { LoginRequest, User } from 'types/apiTypes';
+import LoginRegister from 'services/LoginRegister';
+import { useUserStore } from 'store/userStore';
+import { toast } from 'sonner';
+
+interface ErrorsForm {
+  username?: string;
+  password?: string;
+  error?: string;
+}
+
+interface LoginProps {
+  handleLogin: () => void;
+}
+
+export const useLogin = ({ handleLogin }: LoginProps) => {
+  useEffect(() => {
+    recoverSesionStorage();
+  }, []);
+  const setUser = useUserStore((state) => state.setUser);
+  const setTokenStore = useUserStore((state) => state.setToken);
+  const setIsLogged = useUserStore((state) => state.setIsLogged);
+  const setRefreshToken = useUserStore((state) => state.setRefreshToken);
+
+  const navigate = useNavigate();
+  const [infoUser, setInfoUser] = useState({
+    username: '',
+    password: ''
+  });
+  const [errors, setErrors] = useState<ErrorsForm>({});
+
+  const sendData = async (infoUser: LoginRequest) => {
+    try {
+      const errorsForm: ErrorsForm = validateForm([
+        { name: 'userName', value: infoUser.username, required: true },
+        {
+          name: 'password',
+          value: infoUser.password,
+          required: true,
+          minLength: 8
+        }
+      ]);
+
+      setErrors(errorsForm);
+      if (!errorsForm.username && !errorsForm.password) {
+        await loginUser(infoUser);
+        navigate('/');
+      }
+    } catch (err) {
+      if (err instanceof Error) setErrors({ error: err.message });
+    }
+  };
+
+  const handleSignupClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    handleLogin();
+  };
+
+  const loginUser = async (credentials: LoginRequest): Promise<void> => {
+    const response = await LoginRegister.login(credentials);
+    const { token, user, refreshToken } = response;
+    if (token) {
+      localStorage.setItem('token', JSON.stringify(token)); // Store the user token in local storage
+      saveGlobalStateUser(token, user, refreshToken);
+      toast.success('Login successful', {
+        duration: 2000
+      });
+    } else {
+      throw new Error('Incorrect username or password');
+    }
+  };
+  const saveGlobalStateUser = (
+    token: string,
+    user: User,
+    refreshToken: string
+  ) => {
+    setTokenStore(token);
+    setUser(user);
+    setRefreshToken(refreshToken);
+    setIsLogged(true);
+    saveSesionStorage(user, refreshToken);
+  };
+  const saveSesionStorage = (user: User, refreshToken: string) => {
+    const sesionID = `${user.id};${user.username};${user.email};${refreshToken}`;
+    sessionStorage.setItem('sesionId', sesionID);
+  };
+  const recoverSesionStorage = () => {
+    const sesionID = sessionStorage.getItem('sesionId');
+    const localStoragetoken = localStorage.getItem('token');
+    if (sesionID !== null && localStoragetoken !== null) {
+      const token = localStoragetoken;
+      const sesion = sesionID.split(';', 4);
+      const user = {
+        id: sesion[0],
+        username: sesion[1],
+        email: sesion[2],
+        active: true,
+        updateDate: '',
+        softDeleteDate: ''
+      };
+      const refreshToken = sesion[3];
+      saveGlobalStateUser(token, user, refreshToken);
+    }
+  };
+  return {
+    infoUser,
+    errors,
+    sendData,
+    handleSignupClick,
+    setInfoUser,
+    recoverSesionStorage
+  };
+};
