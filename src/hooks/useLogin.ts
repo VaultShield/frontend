15003553717ebe2a0a47d9d1672b6bf71fc763d8
Othamae <vero.gconesa@gmail.com>
2024-05-03 19:58:14 +0200

    feat: Separated storage logic from useLogin

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { validateForm } from 'utils/validations';
-import { LoginRequest, User } from 'types/apiTypes';
 import LoginRegister from 'services/LoginRegister';
-import { useUserStore } from 'store/userStore';
 import { toast } from 'sonner';
+import { LoginRequest } from 'types/apiTypes';
+import { validateForm } from 'utils/validations';
+import { useStorage } from './useStorage';
 
 interface ErrorsForm {
   username?: string;
@@ -17,14 +17,7 @@ interface LoginProps {
 }
 
 export const useLogin = ({ handleLogin }: LoginProps) => {
-  useEffect(() => {
-    recoverSesionStorage();
-  }, []);
-  const setUser = useUserStore((state) => state.setUser);
-  const setTokenStore = useUserStore((state) => state.setToken);
-  const setIsLogged = useUserStore((state) => state.setIsLogged);
-  const setRefreshToken = useUserStore((state) => state.setRefreshToken);
-
+  const { saveGlobalStateUser } = useStorage();
   const navigate = useNavigate();
   const [infoUser, setInfoUser] = useState({
     username: '',
@@ -74,45 +67,12 @@ export const useLogin = ({ handleLogin }: LoginProps) => {
       throw new Error('Incorrect username or password');
     }
   };
-  const saveGlobalStateUser = (
-    token: string,
-    user: User,
-    refreshToken: string
-  ) => {
-    setTokenStore(token);
-    setUser(user);
-    setRefreshToken(refreshToken);
-    setIsLogged(true);
-    saveSesionStorage(user, refreshToken);
-  };
-  const saveSesionStorage = (user: User, refreshToken: string) => {
-    const sesionID = `${user.id};${user.username};${user.email};${refreshToken}`;
-    sessionStorage.setItem('sesionId', sesionID);
-  };
-  const recoverSesionStorage = () => {
-    const sesionID = sessionStorage.getItem('sesionId');
-    const localStoragetoken = localStorage.getItem('token');
-    if (sesionID !== null && localStoragetoken !== null) {
-      const token = localStoragetoken;
-      const sesion = sesionID.split(';', 4);
-      const user = {
-        id: sesion[0],
-        username: sesion[1],
-        email: sesion[2],
-        active: true,
-        updateDate: '',
-        softDeleteDate: ''
-      };
-      const refreshToken = sesion[3];
-      saveGlobalStateUser(token, user, refreshToken);
-    }
-  };
+
   return {
     infoUser,
     errors,
     sendData,
     handleSignupClick,
-    setInfoUser,
-    recoverSesionStorage
+    setInfoUser
   };
 };
