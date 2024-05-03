515c1e570effed25a7aa670954ef51bbf83ed038
Othamae <vero.gconesa@gmail.com>
2024-05-03 19:57:23 +0200

    feat: Created useStorage

---
+++ src/hooks/useStorage.ts
@@ -0,0 +1,46 @@
+import { useUserStore } from 'store/userStore';
+import { User } from 'types/apiTypes';
+
+export const useStorage = () => {
+  const setUser = useUserStore((state) => state.setUser);
+  const setTokenStore = useUserStore((state) => state.setToken);
+  const setIsLogged = useUserStore((state) => state.setIsLogged);
+  const setRefreshToken = useUserStore((state) => state.setRefreshToken);
+  const saveSesionStorage = (user: User, refreshToken: string) => {
+    sessionStorage.removeItem('sesionId');
+    const sesionID = `${user.id};${user.username};${user.email};${refreshToken}`;
+    sessionStorage.setItem('sesionId', sesionID);
+  };
+  const saveGlobalStateUser = (
+    token: string,
+    user: User,
+    refreshToken: string
+  ) => {
+    setTokenStore(token);
+    setUser(user);
+    setRefreshToken(refreshToken);
+    setIsLogged(true);
+    saveSesionStorage(user, refreshToken);
+  };
+
+  const recoverSesionStorage = () => {
+    const sesionID = sessionStorage.getItem('sesionId');
+    const localStoragetoken = localStorage.getItem('token');
+    if (sesionID !== null && localStoragetoken !== null) {
+      const token = localStoragetoken;
+      const sesion = sesionID.split(';', 4);
+      const user = {
+        id: sesion[0],
+        username: sesion[1],
+        email: sesion[2],
+        active: true,
+        updateDate: '',
+        softDeleteDate: ''
+      };
+      const refreshToken = sesion[3];
+      saveGlobalStateUser(token, user, refreshToken);
+    }
+  };
+
+  return { saveSesionStorage, recoverSesionStorage, saveGlobalStateUser };
+};
