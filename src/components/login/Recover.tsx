45dcb9396c4eb3a2ed5002cab424d8d56038b6c8
Busta <josuebustatr@gmail.com>
2024-05-04 17:04:47 +0200

    Fix: Recover Responsive

--- src/components/login/Recover.tsx
+++ src/components/login/Recover.tsx
@@ -9,8 +9,8 @@ export function Recover({ onClose }: RecoverProps) {
   const [userCorrect, setUserCorrect] = useState('');
 
   return (
-    <div className="absolute flex justify-center items-center right-0 top-0 h-screen w-screen  z-50 ">
-      <div className="bg-white text-whitebg w-full h-full flex flex-col justify-center rounded-lg items-center p-10 space-y-5">
+    <div className="absolute flex justify-center items-center right-0 top-0 h-screen  w-screen  z-50 ">
+      <div className="bg-white text-whitebg w-full h-full flex flex-col justify-center rounded-lg items-center p-6 space-y-5">
         <div className="w-full flex justify-start">
           <div
             onClick={() => {
@@ -23,18 +23,18 @@ export function Recover({ onClose }: RecoverProps) {
         </div>
 
         {userCorrect === 'seed' ? (
-          <div className="w-full h-full flex flex-col justify-center items-center space-y-5">
+          <div className="w-full h-full  flex flex-col justify-center items-center space-y-5">
             <div className="font-bold text-2xl text-black">
               Enter All Words!
             </div>
             <div className="text-gray-400">
               Enter the words we gave you when you registered in the same order.
             </div>
-            <div className="grid grid-cols-5 gap-4 p-5 bg-primary bg-opacity-15 rounded-lg">
+            <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 p-5 bg-primary bg-opacity-15 rounded-lg">
               {Array.from({ length: 15 }).map((_, index) => (
                 <div
                   key={index}
-                  className="flex items-center text-center rounded-lg w-40 space-x-2 text-black"
+                  className="flex items-center text-center rounded-lg sm:w-40 space-x-2 text-black"
                 >
                   <div className="text-black w-4">{index + 1}</div>
                   <input
@@ -45,7 +45,7 @@ export function Recover({ onClose }: RecoverProps) {
               ))}
             </div>
 
-            <div className="w-[30rem] ">
+            <div className="w-full max-w-[30rem] ">
               <button
                 onClick={() => setUserCorrect('password')}
                 className="bg-primary text-white h-12 w-full rounded-full font-semibold"
@@ -59,7 +59,7 @@ export function Recover({ onClose }: RecoverProps) {
             <div className="font-bold text-black text-2xl">
               Enter your new password
             </div>
-            <div className="w-[30rem] flex items-center">
+            <div className="w-full max-w-[30rem] flex items-center">
                 <KeyRoundedIcon className="absolute ml-4 text-primary" />{' '}
                 <input
                   type="text"
@@ -67,7 +67,7 @@ export function Recover({ onClose }: RecoverProps) {
                   placeholder="New Password"
                 />
               </div>{' '}
-              <div className="w-[30rem] ">
+              <div className="w-full max-w-[30rem] ">
                 <button
                   onClick={() => setUserCorrect('')}
                   className="bg-primary text-white h-12 w-full rounded-full font-semibold"
@@ -81,7 +81,7 @@ export function Recover({ onClose }: RecoverProps) {
               <div className="font-bold text-2xl text-black">
                 Enter your username
               </div>
-              <div className="w-[30rem] flex items-center">
+              <div className="w-full max-w-[30rem] flex items-center">
                   {' '}
                   <PersonRoundedIcon className="absolute ml-4 text-primary" />
                   <input
@@ -90,7 +90,7 @@ export function Recover({ onClose }: RecoverProps) {
                     placeholder="Username"
                   />
                 </div>
-                <div className="w-[30rem] ">
+                <div className="w-full max-w-[30rem]">
                   <button
                     onClick={() => setUserCorrect('seed')}
                     className="bg-primary text-white h-12 w-full rounded-full font-semibold"
