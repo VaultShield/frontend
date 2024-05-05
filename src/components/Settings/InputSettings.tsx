bd1bc2a66be7c21af581def202983aaf2fe81b4e
Othamae <vero.gconesa@gmail.com>
2024-05-05 16:53:35 +0200

    feat: Added view/ocult password

--- src/components/Settings/InputSettings.tsx
+++ src/components/Settings/InputSettings.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import React, { useState } from 'react';
 
 interface InputSettingsProps {
   value: string;
@@ -21,19 +23,28 @@ const InputSettings = ({
   type = 'text',
   placeholder
 }: InputSettingsProps) => {
+  const [view, setView] = useState(false);
   return (
     <div className="flex flex-row w-full gap-4">
       <div className="relative w-full flex items-center">
         <div className="absolute ml-4 text-primary">{icon}</div>
         <input
           name={name}
-          type={type}
+          type={!view ? type : 'text'}
           className="bg-primary text-primary bg-opacity-15  rounded-full h-12 w-full pl-12 outline-none placeholder:text-primary placeholder:text-opacity-75 placeholder:italic"
           value={value}
           disabled={!isEditing}
           onChange={handleChange}
           placeholder={placeholder}
         ></input>
+        {type === 'password' && (
+          <div
+            className="absolute end-0 mr-4 text-primary cursor-pointer"
+            onClick={() => setView(!view)}
+          >
+            {view ? <Visibility /> : <VisibilityOff />}
+          </div>
+        )}
         {error && <p className="text-red-500">{error}</p>}
       </div>
     </div>
