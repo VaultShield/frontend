070620cee19ba63b9ecd0939a9833b4fe02fa68a
Othamae <vero.gconesa@gmail.com>
2024-05-05 17:08:18 +0200

    feat: keep same size for icon

--- src/components/Settings/ButtonIcon.tsx
+++ src/components/Settings/ButtonIcon.tsx
@@ -16,7 +16,7 @@ const ButtonIcon = ({
     <button
       type={type}
       onClick={handleClick}
-      className="md:h-12 md:w-12 w-10 h-10 rounded-lg border-2 border-primary  text-primary border-opacity-15 hover:bg-primary hover:text-white flex justify-center items-center cursor-pointer z-20"
+      className=" w-12 h-12 rounded-lg border-2 border-primary  text-primary border-opacity-15 hover:bg-primary hover:text-white flex justify-center items-center cursor-pointer z-20"
     >
       {icon}
       {text}
