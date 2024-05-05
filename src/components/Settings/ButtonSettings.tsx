9db3842dc69d8fb988a7da0e2f60abc4fd484e11
Othamae <vero.gconesa@gmail.com>
2024-05-05 15:57:52 +0200

    feat: Created ButtonSettings component

---
+++ src/components/Settings/ButtonSettings.tsx
@@ -0,0 +1,23 @@
+interface ButtonSettingsProps {
+  handleClick?: () => void;
+  text: string;
+  type?: 'button' | 'submit' | 'reset';
+}
+
+const ButtonSettings = ({
+  handleClick,
+  text,
+  type = 'button'
+}: ButtonSettingsProps) => {
+  return (
+    <button
+      type={type}
+      onClick={handleClick}
+      className="bg-primary w-full h-12 rounded-full text-white  "
+    >
+      {text}
+    </button>
+  );
+};
+
+export default ButtonSettings;
